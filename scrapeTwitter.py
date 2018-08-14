72d876882a33db7a7617a486bcbbbba1f2f305f1
aukslius12 <aukslius@gmail.com>
2018-08-14 11:13:58 +0300

    Scraping on AWS

--- scrapeTwitter.py
+++ scrapeTwitter.py
@@ -1,72 +1,13 @@
-import twint
+'''
+As most of twitter scrapers are buggy as fuck, I will switch to AWS and comand line.
 
-class scrapeTweets:
-    '''
-    Documented, watered down version of twint.
+    * Virtual machine - Amazon Linux 2 AMI (HVM), SSD Volume Type
     
-    Returns None.
-    ------
-    Parameters, attributes or whatever:
-    
-    since (str): date to start searching from.
-    
-    until (str): date to end the search on.
-    
-    language (str): get tweets which are written in a specific language only. (Doesn't work 100% correctly in classifying).
-    
-    store_csv (bool): if true, saves a csv file.
-    
-    csv_fieldnames (list of (str)): parameters to include in csv file. Further info in twint's GitHub.
-    
-    csv_output (str): filename of output file.
-    
-    '''
-    def __init__(
-        self,
-        search,
-        since = None,
-        until = None,
-        language = 'en',
-        store_csv = True,
-        csv_fieldnames = ["id", "user_id", "username", "date", "time", "timezone", "tweet", "replies", "retweets", "likes"],
-        csv_output = 'twitter_data.csv'):
-    
-        self.term = search
-        self.since = since
-        self.until = until
-        self.lang = language
-        self.store = store_csv
-        self.csv_fn = csv_fieldnames
-        self.out = csv_output
-        self.search()
-        
-    def search(self):
-        '''
-        Executes twint with parameters specified in the class.
-        
-        Returns None.
-        ------
-        Parameters:
-        
-        None
-        '''
-        # Configure base parameters.
-        c = twint.Config()
-        c.Search = self.term
-        if self.since != None:
-            c.Since = self.since
-        if self.until != None:
-            c.Until = self.until
-        c.Lang = self.lang
-        
-        # Configure storing.
-        c.Store_csv = self.store
-        c.Custom = self.csv_fn
-        c.Output = self.out
-        
-        # Run the scraper.
-        twint.run.Search(c)
-        
-        return None
-    
-scrapeTweets(search='lithuania') # Can be deleted if you want to import it as a class. 
+    * run this:
+    twitterscraper "lithuania OR Lithuania OR Lietuva OR lietuva OR LTU OR ltu" -o twitter_data.json --lang en
+
+    * Transfer the result file via Filezilla.
+
+
+This file will be removed in the future.
+'''
